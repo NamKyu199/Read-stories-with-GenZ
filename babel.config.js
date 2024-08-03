7c65794292e1b023c18857e4419970b781ac45d5
namkyu <hoangthanhnam159@gmail.com>
2024-08-03 10:43:39 +0700

    file base

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,28 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          // This has to be mirrored in tsconfig.json
+          '^~(.+)': './src/\\1',
+        },
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.js',
+          '.jsx',
+          '.json',
+          '.tsx',
+          '.ts',
+          '.native.js',
+        ],
+      },
+    ],
+  ],
+  // - Add this -
 };
